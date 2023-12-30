7c3fb2b81e61699ec8406600dd0993315387be29
nvn1176211 <nvnit1176211@gmail.com>
2023-12-30 21:49:06 +0700

    fix switch route delay

--- src/main.js
+++ src/main.js
@@ -4,23 +4,26 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Events from "./components/Events.vue"
+import Login from "./components/Login.vue"
+import Event from "./components/Event.vue"
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: '/',
       name: 'events',
-      component: () => import('./components/Events.vue')
+      component: Events
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('./components/Login.vue')
+      component: Login
     },
     {
       path: '/events/:id',
       name: 'event',
-      component: () => import('./components/Event.vue')
+      component: Event
     },
   ]
 })
