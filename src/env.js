651ab8d333bc0cba778b96ddb7725ad639bd3a61
nvn1176211 <nvnit1176211@gmail.com>
2024-03-03 13:38:58 +0700

    update api host

--- src/env.js
+++ src/env.js
@@ -1,7 +1,7 @@
 // window.API_BASE = 'http://127.0.0.1:8000/api';
 // window.API_HOST = 'http://127.0.0.1:8000';
-// window.API_BASE = 'https://nvn1.000webhostapp.com/api';
-// window.API_HOST = 'https://nvn1.000webhostapp.com';
-window.API_BASE = 'http://nvnwh.atwebpages.com/api';
-window.API_HOST = 'http://nvnwh.atwebpages.com';
+window.API_BASE = 'https://nvnwh.000webhostapp.com/api';
+window.API_HOST = 'https://nvnwh.000webhostapp.com';
+// window.API_BASE = 'http://nvnwh.atwebpages.com/api';
+// window.API_HOST = 'http://nvnwh.atwebpages.com';
 export default { API_BASE };
